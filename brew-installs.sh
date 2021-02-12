d0f0e5327c40f0c1534d66286610391d0cc697dd
Polovincev Roman <polovincev@gmail.com>
2021-02-13 00:55:34 +0300

    install brew

---
+++ brew-installs.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+#Check if Homebrew is installed
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    echo 'Please install Homebrew by running the following command: /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
+else
+    brew update
+fi
+
+#Upgrade installed formulae
+brew upgrade
+
+# Save Homebrew’s installed location
+BREW_PREFIX=$(brew --prefix)
+
+#Tools
+brew install wget --with-iri
+brew install git
+brew install zsh
+
+#Apps
+brew install --cask google-chrome
+brew install --cask iterm2
+brew install --cask vlc
+brew install --cask visual-studio-code
+brew install --cask zoom
+brew install --cask fork 
+
+# Remove outdated versions from the cellar.
+brew cleanup
